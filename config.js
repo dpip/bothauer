0f0642efb02e1db7a80391af8719d0c29e395e3d
Daniel Pipkin <danielpipkin@KristenCollossoMBPro.local>
2017-12-26 14:13:24 -0500

    initail commit / creates bot / config file and package json

---
+++ config.js
@@ -0,0 +1,14 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+
+module.exports = {
+  consumer_key: '0quM3cTrDmXz3SwrghkXUiJhU',
+  consumer_secret: '6nKNuaKLCogUa6Hm3dYQzztWttH4xQH4g7iK6YH1mholBQU8dD',
+  access_token: '814171278-7OOTLHCQbxahuZY2kxiEc91UFg5sQb4C3tlDkPC1',
+  access_token_secret: 'XK9PY0adJhooau7BZufye8VcLciTeRjNOBAWtoTIGfF0s'
+}
